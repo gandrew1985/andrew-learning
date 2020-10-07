b7f7a348e4d293761340bb06c844ec96b2d2568d
gandrew1985 <gandrew@op.pl>
2020-10-07 13:16:39 +0100

    First example

--- andrzej-testing/src/main/java/com/andrzej/testing/TestingMain.java
+++ andrzej-testing/src/main/java/com/andrzej/testing/TestingMain.java
@@ -4,5 +4,6 @@ public class TestingMain {
 
     public static void main(String[] args) {
         System.out.println("Modul 6 wprowadznie do testowania");
+        System.out.println("Zaczynamy");
     }
 }
