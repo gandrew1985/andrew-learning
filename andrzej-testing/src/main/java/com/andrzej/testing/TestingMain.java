e76781d50ec7523b1c4662a9d79006151fb22685
gandrew1985 <gandrew@op.pl>
2020-10-07 13:10:40 +0100

    First try

---
+++ andrzej-testing/src/main/java/com/andrzej/testing/TestingMain.java
@@ -0,0 +1,8 @@
+package com.andrzej.testing;
+
+public class TestingMain {
+
+    public static void main(String[] args) {
+        System.out.println("Modul 6 wprowadznie do testowania");
+    }
+}
