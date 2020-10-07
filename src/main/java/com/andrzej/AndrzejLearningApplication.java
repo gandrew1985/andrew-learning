93bcea3e00e909bba0e03df8a5677d715f0194fb
gandrew1985 <gandrew.golab@gmail.com>
2020-10-07 12:52:57 +0100

    Starting new project

---
+++ src/main/java/com/andrzej/AndrzejLearningApplication.java
@@ -0,0 +1,13 @@
+package com.andrzej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndrzejLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndrzejLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrzej/AndrzejLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrzej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndrzejLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
